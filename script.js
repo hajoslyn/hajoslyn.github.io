b302fca429c9f34772fc294a1f628c8d96dbca3f
Hjoslyn <heatherjoslyn@comcast.net>
2016-02-28 14:59:05 -0500

    Sunday version

---
+++ script.js
@@ -0,0 +1,32 @@
+// function turtleFact() {
+// 	alert("Some turtles can live longer than humans");
+// }
+
+// 	turtleFact();
+
+// function temptGang (gangName) {
+// 	alert(gangName + "Come out and play!")
+// }
+// 	temptGang();
+
+// 	function addNumbers(num1, num2) {
+// 		alert(num1 + num2)
+// 	}
+
+// 	addNumbers(10, 15);
+
+	function square(number) {
+		return number * number;
+	}
+
+	function displaySquare(squaredNumber) {
+		console.log("Your number is " + squaredNumber);
+
+	displaySquare(square(3));
+
+
+	var pageNode = document.getElementsByTagName('body') [0];
+		pageNode.innerHTML = <h1>"Oh no! My page has been hijacked!";</h1>
+
+		var pageNode = document.getElementsByTagName('open-title') [0];
+		title.innerHTML = "Thanks for attending class!";
